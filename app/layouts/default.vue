31941683be863f15acc6390f528d9e113034a0cf
Kacper Lechicki <klechicki.wrk@gmail.com>
2025-04-06 19:18:45 +0200

    feat: default layout | first composable

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout-container">
+    <main class="main-content">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.layout-container {
+  height: calc(100vh - 4rem);
+  background: var(--ui-bg);
+}
+
+.main-content {
+  margin-top: 4rem;
+  height: 100%;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <UContainer class="page-container">
+    <h1 class="title">Nuxt Chat</h1>
+    <UButton to="/chat">Start Chat</UButton>
+  </UContainer>
+</template>
+
+<style lang="css" scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  height: 100vh;
+}
+
+.title {
+  font-size: 2.25rem;
+  font-weight: bold;
+}
+</style>
